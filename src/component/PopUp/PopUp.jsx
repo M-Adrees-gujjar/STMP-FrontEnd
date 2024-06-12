703db0790d49f9356fd69874090ad8e0ffb556a8
Muhammad Adrees <seebiz.bpt0623.dev04@gmail.com>
2024-06-12 19:57:03 +0500

    Updation

--- src/component/PopUp/PopUp.jsx
+++ src/component/PopUp/PopUp.jsx
@@ -1,6 +1,7 @@
 
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 export default function PopUp(props) {
   const {open , setOpen} = props
@@ -43,21 +44,24 @@ export default function PopUp(props) {
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
+                  <Link
+                    to="/stddashboard"
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
-                    onClick={() => location.replace('/stddashboard')}
+                    // onClick={() => location.replace('/stddashboard')}
                   >
                     Student
-                  </button>
-                  <button onClick={()=>location.replace("/signIn")}
+                  </Link>
+                  <Link 
+                  // onClick={()=>location.replace("/signIn")}
+                  to="/signIn"
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     // onClick={() => setOpen(false)}
                     data-autofocus
                   >
                     Teacher
-                  </button>
+                  </Link>
                 </div>
               </DialogPanel>
             </TransitionChild>
